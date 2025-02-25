5cd3b09bc0a10047d57bef29417675d7fcb9335c
Victor Eduardo <VictorOdarp@gmail.com>
2025-02-24 22:41:11 -0300

    Estruturando as classes Menu e Game

---
+++ code/Menu.py
@@ -0,0 +1,8 @@
+import pygame
+class Menu:
+
+    def __init__(self, window):
+        self.window = window
+
+    def run(self, ):
+        pass
--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import pygame
+from code.Game import Game
 
-pygame.init()
-window = pygame.display.set_mode(size = (600, 480))
+game = Game()
 
-while True:
-    # Check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+game.run()

f93255dbb5a1a2c0ff50fa6dadb1ea996b2e9b76
Victor Eduardo <VictorOdarp@gmail.com>
2025-03-05 22:16:11 -0300

    Level 1 implementação Parallax

--- code/Background.py
+++ code/Background.py
@@ -1,3 +1,4 @@
+from code.Const import WIN_WIDTH, ENTITY_SPEED
 from code.Entity import Entity
 
 
@@ -6,4 +7,7 @@ class Background(Entity):
         super().__init__(name, position)
 
     def move(self, ):
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.rect.right == 0:
+            self.rect.left = WIN_WIDTH
         pass
